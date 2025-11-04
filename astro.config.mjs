ce76339906768f99a0bec1cca11be35d411d7902
Denis Cazzulo <deniscazzulo@icloud.com>
2025-11-04 16:17:37 +0100

    correzioni vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.sergiocontegiacomo.it',
+	output: 'server',
+	adapter: vercel(),
 	integrations: [tailwind({ configFile: './tailwind.config.cjs' }), sitemap()],
 	scopedStyleStrategy: 'where',
 });
