c2667c5adfcaf67e49708841a0677a4b7d8b7fd1
Denis Cazzulo <deniscazzulo@icloud.com>
2025-11-26 23:51:56 +0100

    aggiornamento per seo e ottimizzazioni

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,28 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
+import playformCompress from '@playform/compress';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.sergiocontegiacomo.it',
-	output: 'server',
-	adapter: vercel(),
-	integrations: [tailwind({ configFile: './tailwind.config.cjs' }), sitemap()],
-	scopedStyleStrategy: 'where',
+    site: 'https://www.sergiocontegiacomo.it',
+    output: 'server',
+    adapter: vercel(),
+    integrations: [
+      tailwind({ configFile: './tailwind.config.cjs' }),
+      sitemap(),
+      playformCompress(),
+      robotsTxt({
+        policy: [
+          { 
+						userAgent: 'Googlebot',
+						allow: '/',
+						disallow: ['/admin', '/admin/*'],
+						crawlDelay: 2,
+					 },
+        ],
+      }),
+		],
+    scopedStyleStrategy: 'where',
 });
